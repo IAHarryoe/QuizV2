9ff26df692fabb7326dee601490ad3ef699cce6e
Harryoe <51862322+IAHarryoe@users.noreply.github.com>
2022-04-21 16:28:23 -0600

    Started basic stuff to make it work
    Still nonfunctional though

---
+++ src/server.py
@@ -0,0 +1,15 @@
+import websockets
+import asyncio
+
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        print(message)
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
